37ab0210927f8bce4872cb0abf80ea2529e494dd
Evgeniia <angela35@mail.ru>
2022-08-20 14:19:07 +0300

    Task8

---
+++ Семинар1/Task_8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int num=int.Parse(Console.ReadLine());
+int current=2;
+    
+while (current<=num)
+{ 
+   if (current%2==0)
+    {
+         Console.Write($"{current} ");
+    }    
+       current++; 
+    
+    }
