273b6feab6edea371df520f6c207ee4aa7f11bdd
Evgeniia <angela35@mail.ru>
2022-08-31 16:47:33 +0300

    Task27

---
+++ Семинар4/Task_27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11   82 -> 10   9012 -> 12
+
+int Summa(int amount)
+{
+    int result=0;
+    while (amount!=0)
+    {
+       result+=amount%10;
+       amount=amount/10;
+    }
+    return result;
+}
+Console.Write("Введите число: ");
+int col=int.Parse(Console.ReadLine());
+Console.WriteLine($"{Summa(col)}");
