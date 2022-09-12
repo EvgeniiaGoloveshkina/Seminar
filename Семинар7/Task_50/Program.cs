cd6c27d4547c990a38a2873fe9db28cb826f42ba
Evgeniia <angela35@mail.ru>
2022-09-12 14:13:57 +0300

    Task50

---
+++ Семинар7/Task_50/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает число и генерирует случайный двумерный массив, и возвращает индексы этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] GetArray(int row, int column)
+{
+    int[,] result = new int[row, column];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(-20, 20);
+        }
+    }
+    return result;
+}
+void SearchIndex(int[,] result, int number)
+{
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            if (result[i, j] == number)
+            {
+                Console.Write($"Позиция числа {number}: [{i}, {j}] ");
+                return;
+            };
+        }
+    }
+    Console.Write($"Числа {number} в массиве нет ");
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+int[,] myArray = GetArray(rows, columns);
+Console.WriteLine("Массив: ");
+PrintArray(myArray);
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+SearchIndex(myArray, number);
